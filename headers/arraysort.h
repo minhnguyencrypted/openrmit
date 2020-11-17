8ab01019f458ca7e07363ce39a0d2e7cc7345a95
Minh Nguyen <minhnguyen1205@outlook.com>
2020-11-17 21:05:16 +0700

    Update Bubble Sort function

--- headers/arraysort.h
+++ headers/arraysort.h
@@ -1,31 +1,41 @@
 //Header file (*.h)
 //Array of Intergers Sorting functions 
 //Written by Minh N. Nguyen (s3877222)
-//Licensed under GNU GPL v3.0, visit https://www.gnu.org/licenses/gpl-3.0.en.html for more info
+//Licensed under GNU General Public License v3.0, visit https://www.gnu.org/licenses/gpl-3.0.en.html for more info
 
 #include <stdio.h>
 
 //Functions declarations
-//Bubble sort function
-void bubblesort (int arraysize, int* array);
+/*
+    Technically, functions should take these arguments, repectively:
+	int sortsize: the number of elements to be sorted, starting from array pointer.
+	int* array: the pointer to the initial elements of a sequence of an array.
+	int order: the integer indicates the order of the sort ("+" value for ascending and "-" value for descending).
+		   An inappropriate integer choice may cause unwanted function behaviour, keep that in mind.
+*/
 
-//Fucntions definitions
-//Damn none of 'em are pure
+//Bubble sort function
+void bubblesort (int sortsize, int* array, int order);
+
+//Functions definitions
+//Damn, none of 'em are pure
 //#saveHaskell
 
 //Bubble sort function 
-void bubblesort (int arraysize, int *array) {
-    for (int i=0; i<arraysize; i++) {
-	for (int j=i+1; j<arraysize; j++) {
-	    //if latter arrar element is greater than the former then swap pos
-	    if (array[j] < array[i]) {
-		int swp = array[j];
-		array[j] = array[i];
-		array[i] = swp;
+void bubblesort (int sortsize, int *array, int order) {
+    for (int i=0; i<sortsize; i++) {
+	for (int j=i+1; j<sortsize; j++) {
+	    if ((*(array + j)*order) < (*(array + i)*order)) {
+		int swp = *(array + i);
+		*(array + i) = *(array + j);
+		*(array + j) = swp;
 	    }
-	   
 	}
     }
 }
 
 //End of Header file
+//I'm so sorry if there's any typos, my bad
+//God danm stiky keys
+//Sincerely,
+//Minh N. Nguyen Mr. :)))
